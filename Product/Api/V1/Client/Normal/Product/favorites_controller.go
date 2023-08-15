4f2c972e13bf5d4b792476c32795efadd5ee87a1
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2023-08-15 04:31:03 +0300

    add favorites & feed controller

--- Product/Api/V1/Client/Normal/Product/favorites_controller.go
+++ Product/Api/V1/Client/Normal/Product/favorites_controller.go
@@ -1,15 +1,30 @@
 package Product
 
 import (
-	"Backend/Product/Services/Internal/Product"
+	"Backend/Core/Constants/Keys/ContextKeys"
+	"Backend/Core/Constants/Keys/ErrorKeys"
+	"Backend/Core/Utilities/Helpers"
+	"Backend/Core/Utilities/Responses"
+	"Backend/Product/Model/User"
+	"Backend/Product/Services/Internal/Feed"
 	"net/http"
 )
 
 type FavoritesController struct {
-	Service Product.IProductService
+	Service Feed.IFeedService
 }
 
 func (f FavoritesController) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	//TODO implement me
-	panic("implement me")
+	user := request.Context().Value(ContextKeys.UserKey).(*User.User)
+	page, limit, err := Helpers.StripPaginationFromUrl(request)
+	if err != nil {
+		Responses.InvokeBadRequest(writer, ErrorKeys.InvalidPagination)
+		return
+	}
+	products, err := f.Service.Favorites(int(page), int(limit), user)
+	if err != nil {
+		Responses.InvokeInternalServerError(writer, err.Error())
+		return
+	}
+	Responses.InvokeSuccess(writer, products)
 }
--- Product/Api/V1/Client/Normal/Product/feed_controller.go
+++ Product/Api/V1/Client/Normal/Product/feed_controller.go
@@ -1,7 +1,27 @@
 package Product
 
-import "Backend/Product/Services/Internal/Product"
+import (
+	"Backend/Core/Constants/Keys/ErrorKeys"
+	"Backend/Core/Utilities/Helpers"
+	"Backend/Core/Utilities/Responses"
+	"Backend/Product/Services/Internal/Feed"
+	"net/http"
+)
 
 type FeedController struct {
-	Service Product.IProductService
+	Service Feed.IFeedService
+}
+
+func (self FeedController) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	page, limit, err := Helpers.StripPaginationFromUrl(request)
+	if err != nil {
+		Responses.InvokeBadRequest(writer, ErrorKeys.InvalidPagination)
+		return
+	}
+	products, err := self.Service.Feed(int(page), int(limit))
+	if err != nil {
+		Responses.InvokeInternalServerError(writer, err.Error())
+		return
+	}
+	Responses.InvokeSuccess(writer, products)
 }
